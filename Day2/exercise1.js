d924671fb09007670d25d96543f4adeeae54d2a3
Admin <admin@CarbonFi-MBP01.local>
2026-07-05 15:50:28 +0100

    Question 1-10

--- Day2/exercise1.js
+++ Day2/exercise1.js
@@ -12,3 +12,22 @@ console.log(challenge.toUpperCase())
 
 //Question 5
 console.log(challenge.toLowerCase())
+
+//Question 6
+console.log(challenge.substr(0, 2))
+console.log(challenge.substring(0, 2))
+
+//Question 7
+console.log(challenge.slice(3))
+
+
+//Question 8
+console.log(challenge.includes('Script'))
+
+//Question 9
+console.log(challenge.split())
+
+//Question 10
+console.log(challenge.split(' '))
+
+//Question 11
