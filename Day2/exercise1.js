5344fefcaee177ab88d776a29d46b0b07658e852
Admin <admin@CarbonFi-MBP01.local>
2026-07-05 15:37:29 +0100

    Day 2: exercises1

---
+++ Day2/exercise1.js
@@ -0,0 +1,14 @@
+//Question 1
+let challenge = "30 Days of JavaScript"
+
+//Question 2
+console.log(challenge)
+
+//Question 3
+console.log(challenge.length)
+
+//Question 4
+console.log(challenge.toUpperCase())
+
+//Question 5
+console.log(challenge.toLowerCase())
